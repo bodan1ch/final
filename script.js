8ef0bea5d58d80d0c268ac49d1e71671f00c5c70
Bodan1ch <ostrocherevy@gmail.com>
2025-02-17 14:37:08 +0200

    up3

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-document.addEventListener("scroll", function() {
-    const sections = document.querySelectorAll(".about, .mission, .vision");
-    sections.forEach(section => {
-      const rect = section.getBoundingClientRect();
-      if (rect.top < window.innerHeight - 100) { 
-        section.classList.add("visible");
-      }
-    });
+document.addEventListener("scroll", function () {
+  const sections = document.querySelectorAll(".about, .mission, .vision");
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      section.classList.add("visible");
+    }
   });
-  
+});
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 200) {
+    scrollToTopBtn.classList.remove('hidden');
+  } else {
+    scrollToTopBtn.classList.add('hidden');
+  }
+});
+
+
+scrollToTopBtn.addEventListener('click', function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+}
